71e4fa417ab23c87eaddb08116f8c379f4432f51
emmarcrg <160240686+emmarcrg@users.noreply.github.com>
2024-11-08 14:16:14 +0100

    Analyse metadata

---
+++ src/analyse_json_metadata.py
@@ -0,0 +1,19 @@
+def analyse_kw () :
+    #
+    pass
+    
+def analyse_locs () :
+    #
+    pass
+
+def analyse_org () :
+    #
+    pass
+
+def analyse_per () :
+    #
+    pass
+
+def analyse_mis () :
+    #
+    pass
