0a6970dd1060940880791eb27aed709310c90084
emmarcrg <160240686+emmarcrg@users.noreply.github.com>
2024-11-08 14:40:23 +0100

    récupération des keywords

--- src/analyse_json_metadata.py
+++ src/analyse_json_metadata.py
@@ -1,6 +1,22 @@
-def analyse_kw () :
-    #
-    pass
+import json
+
+def analyse_kw (data) :
+    #Récupérer les kws qui se trouvent dans all
+    # Charger le fichier JSON
+    with open(data, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+
+    # Récupérer les éléments dans 'kws'
+    kws_elements = data["metadata"]["all"]["kws"]
+
+    # Inverser le dictionnaire: clé devient valeur, valeur devient clé
+    kws_inverted = {value: key for key, value in kws_elements.items()}
+
+    # Afficher le dictionnaire inversé
+    print(kws_inverted)
+
+    pass    
+
     
 def analyse_locs () :
     #
@@ -17,3 +33,5 @@ def analyse_per () :
 def analyse_mis () :
     #
     pass
+
+analyse_kw('data/fr.sputniknews.africa--20220630--20230630.json')
